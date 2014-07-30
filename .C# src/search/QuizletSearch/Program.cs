ba9e641caeeec983d659364e6d7043b9a1f3fa4f
rhinohq <ryan@kendrickfamily.info>
2014-07-30 09:56:36 +0100

    I sent the wrong file before... ):

---
+++ .C# src/search/QuizletSearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace QuizletSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Finds the search results and then downloads them.
+            WebClient webClient = new WebClient();
+            String url = "https://api.quizlet.com/2.0/search/sets?client_id=RFsBbdeZFt&whitespace=1&q=" + args[0];
+            dynamic result = JObject.Parse(webClient.DownloadString(url));
+
+            //Writes search results to the console.
+            Console.WriteLine(result);
+            int numresults = result.sets[0].term_count;
+            Console.WriteLine(numresults.ToString());
+
+            //Saves search results as a .json file.
+            string json = JsonConvert.SerializeObject(result);
+            String filename = @"C:\Users\ryan\desktop\results\results.json";
+            System.IO.File.WriteAllText(filename, json);
+        }
+    }
+}
