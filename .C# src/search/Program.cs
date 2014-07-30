62b5ddadc3160bd9e4b071eed69c7b458c769e90
rhinohq <ryan@kendrickfamily.info>
2014-07-30 08:54:35 +0100

    Added some comments.

--- .C# src/search/Program.cs
+++ .C# src/search/Program.cs
@@ -13,12 +13,18 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            //Finds the search results and then downloads them.
             WebClient webClient = new WebClient();
             String url = "https://api.quizlet.com/2.0/search/sets?client_id=RFsBbdeZFt&whitespace=1&q=" + args[0];
             dynamic result = JObject.Parse(webClient.DownloadString(url));
+
+            int id = result["sets"][0]["id"];
+
+            //Writes search results to the console.
             Console.WriteLine(result);
             int numresults = result.sets[0].term_count;
             Console.WriteLine(numresults.ToString());
+            //Saves search results as a .json file.
             string json = JsonConvert.SerializeObject(result);
             String filename = @"C:\Users\ryan\desktop\results\" + args[0] + ".json";
             System.IO.File.WriteAllText(filename, json);
