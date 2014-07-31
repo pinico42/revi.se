c669890bd4d3204b7adf08de11b80b189e7be3f3
theminotaur <mxpen26@gmail.com>
2014-07-31 18:13:53 +0100

    A lovely looking but not working new topic page

--- newtopic.php
+++ newtopic.php
@@ -4,18 +4,32 @@ $l = getLayout("basic.layout");
 $l->writeHeader();
 ?>
 <script type='text/javascript'>
-	function keyPress(e){
+	function keyPress(e){ // called when someone types in the textbox
 		var currentQ = String.fromCharCode(e.keyCode);
 		var q = document.getElementById('topicSearch').value + currentQ;
 		$.get('topicsearch.php?q='+q,function(result){
-			document.getElementById('topicSearchResults').innerHTML = result;
+			document.getElementById('topicSearchResults').getElementsByTagName('tbody')[0].innerHTML = result;
 		});
 	}
 </script>
 <div id='search' class='adds'>
 	<h1 class='title'>Search for topics</h1>
 	<input id='topicSearch' onkeypress="return keyPress(event)">
-	<form method='post' id='topicSearchResults'></form>
+	<form method='post' id='topicSearchResults'>
+		<h4>Your Results:</h4>
+		<table cellpadding="0px" cellspacing="0px" id='searchResults'>
+			<thead>
+				<tr class='c5'>
+					<td>Name</td>
+					<td>Description</td>
+					<td>Put in set</td>
+				</tr>
+			</thead>
+			<tbody>
+				
+			</tbody>
+		</table>
+	</form>
 </div>
 <div id='make' class='adds'>
 	<h1 class='title'>Create a topic</h1>
--- topicsearch.php
+++ topicsearch.php
@@ -1,16 +1,49 @@
 <?php
+function writeResult($sqlArray, $i){ // prints the table cell
+	if(is_null($sqlArray)){
+		return ;
+	}
+	$classNum = ($i%2 == 0 ? 'c2':'c3');
+
+	$name = $sqlArray['name'];
+	$description = $sqlArray['descr'];
+	echo "
+		<tr class='$classNum'>
+			<td>$name</td><td>$description</td>
+		</tr>
+	";
+}
+
 include 'private/pwds.php';
 
 $q = $_GET['q'];
 
 $conn = mysqli_connect('localhost',$mysqlUsername,$mysqlPassword,'revise');
 
-$queryText =  "select * from topics where LOWER(name) LIKE ('%$q%');";
-
+$queryText =  "select * from topics where LOWER(name) LIKE ('%$q%') LIMIT 1 OFFSET 0;";
 
 $mquery = mysqli_query($conn, $queryText);
 
 $array = mysqli_fetch_array($mquery, MYSQLI_ASSOC);
 
-var_dump($array);
+$topics = [$array];
+writeResult($array, 1);
+if(!is_null($array)){
+	$l=2;
+	while(!is_null($array)){
+		$o = $l-1;
+		$conn = mysqli_connect('localhost',$mysqlUsername,$mysqlPassword,'revise');
+
+		$queryText =  "select * from topics where LOWER(name) LIKE ('%$q%') LIMIT $l OFFSET $o;";
+
+		$mquery = mysqli_query($conn, $queryText);
+
+		$array = mysqli_fetch_array($mquery, MYSQLI_ASSOC);
+		$topics = array_merge($topics, [$array]);
+		writeResult($array, $l);
+		$l = $l + 1;
+	}
+} else {
+	echo '<tr class="c3"><td colspan="2">No Results</td></tr>';
+}
 ?>
