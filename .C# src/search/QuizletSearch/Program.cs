179560edceb52a6f9d340be5a33f25e3f77b5b57
rhinohq <ryan@kendrickfamily.info>
2014-08-03 10:05:28 +0100

    More Copyright Notices

--- .C# src/search/QuizletSearch/Program.cs
+++ .C# src/search/QuizletSearch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// Copyright © 2014 Ryan Kendrick
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,3 +30,4 @@ namespace QuizletSearch
         }
     }
 }
+
